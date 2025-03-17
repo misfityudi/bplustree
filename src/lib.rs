e1c6221b5fe1212be4a35df81e4063daa27b4cab
Vikas Chahar <misfityudi@gmail.com>
2025-03-17 17:32:05 +0530

    add node enum, which can be of two types internal and leaf, update new associated fn accordingly along with test

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,26 @@
+pub enum BplusTreeNode {
+    Internal {
+        keys: Vec<i32>,
+        children: Vec<Box<BplusTreeNode>>
+    },
+    Leaf {
+        keys: Vec<i32>,
+        values: Vec<String>,
+        next: Option<Box<BplusTreeNode>>
+    }
+}
+
 pub struct BplusTree {
-    pub items: u8,
-    pub children: Vec<BplusTree>,
-    pub parent: Vec<BplusTree>,
+    pub order: usize,
+    pub root: Option<Box<BplusTreeNode>>
 }
 
 impl BplusTree {
-    pub fn new(items: Option<u8>) -> Self {
+    pub fn new(order: Option<usize>) -> Self {
         Self {
-            items: items.unwrap_or(4),
-            children: Vec::new(),
-            parent: Vec::new(),
+            order: order.unwrap_or(4),
+            root: None,
+            
         }
     }
 }
@@ -21,8 +32,6 @@ mod tests {
     #[test]
     fn new_bplustree() {
         let bplustree = BplusTree::new(None);
-        assert_eq!(bplustree.items, 4);
-        assert_eq!(bplustree.children.len(), 0);
-        assert_eq!(bplustree.parent.len(), 0);
+        assert_eq!(bplustree.order, 4);
     }
 }
