cc903bdf3541ba823f8ac6b7e9f55fb656eb1782
Vikas Chahar <misfityudi@gmail.com>
2025-03-17 17:53:00 +0530

    fmt, add inserting into root

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
+#[derive(Debug, PartialEq)]
 pub enum BplusTreeNode {
     Internal {
         keys: Vec<i32>,
-        children: Vec<Box<BplusTreeNode>>
+        children: Vec<Box<BplusTreeNode>>,
     },
     Leaf {
         keys: Vec<i32>,
         values: Vec<String>,
-        next: Option<Box<BplusTreeNode>>
-    }
+        next: Option<Box<BplusTreeNode>>,
+    },
 }
 
+#[derive(Debug)]
 pub struct BplusTree {
     pub order: usize,
-    pub root: Option<Box<BplusTreeNode>>
+    pub root: Option<Box<BplusTreeNode>>,
 }
 
 impl BplusTree {
@@ -20,9 +22,23 @@ impl BplusTree {
         Self {
             order: order.unwrap_or(4),
             root: None,
-            
         }
     }
+
+    pub fn insert(&mut self, key: i32, value: String) {
+        match &mut self.root {
+            Some(node) => Self::insert_into_node(node, key, value, self.order),
+            None => {
+                self.root = Some(Box::new(BplusTreeNode::Leaf {
+                    keys: vec![key],
+                    values: vec![value],
+                    next: None,
+                }))
+            }
+        }
+    }
+
+    fn insert_into_node(node: &mut BplusTreeNode, key: i32, value: String, order: usize) {}
 }
 
 #[cfg(test)]
@@ -33,5 +49,14 @@ mod tests {
     fn new_bplustree() {
         let bplustree = BplusTree::new(None);
         assert_eq!(bplustree.order, 4);
+    }
+
+    #[test]
+    fn insert_into_root() {
+        let mut bplustree = BplusTree::new(None);
+        assert_eq!(bplustree.order, 4);
+
+        bplustree.insert(10, "Ten".to_string());
+        assert_ne!(bplustree.root, None);
     }
 }
